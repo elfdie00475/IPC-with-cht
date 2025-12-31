4ded3f0b73f2070a7392dee52f20d57e73f63a88
Lin Li-Ting <43197587+elfdie00475@users.noreply.github.com>
2025-12-31 14:27:12 +0800

    Refactor IPC header structure and initialization

--- cht_ipc_common.h
+++ cht_ipc_common.h
@@ -8,6 +8,7 @@
 #include <zwp2pagent/cht_p2p_agent_c.h>
 
 #define CHT_IPC_NAME "system_service.ipc"
+#define CHT_IPC_HEADER_SIZE 32
 #define CHT_IPC_STRING_SIZE 256
 
 #ifndef MAKEFOURCC
@@ -17,40 +18,42 @@
 #endif // MAKEFOURCC
 #define CHT_IPC_FOURCC (MAKEFOURCC('C','H','T','1'))
 
-typedef struct cht_ipc_hdr_st {
+typedef struct cht_ipc_msg_st {
     uint32_t u32FourCC;
-    uint16_t u16CmdType;
-    uint16_t u16Id; // bit1: 0: req, 1: res
+    uint32_t u32HdrSize;
     uint32_t u32PayloadSize;
-    int result;
-} stChtIpcHdr;
+    uint16_t u16Headers[CHT_IPC_HEADER_SIZE];
+    // 0: cmd type
+    // 1: msg id
+    // 2: result
+    uint8_t *pu8Payload;
+} stChtIpcMsg;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-static void cht_ipc_hdr_init(stChtIpcHdr *m, uint16_t u16Id, uint16_t u16CmdType)
+static void cht_ipc_msg_init(stChtIpcMsg *m, uint16_t u16MsgId, uint16_t u16CmdType)
 {
     m->u32FourCC = CHT_IPC_FOURCC;
-    m->u16CmdType = u16CmdType;
-    m->u16Id = u16Id;
+    m->u16Headers[0] = u16CmdType; // 0: cmd type
+    m->u16Headers[1] = u16MsgId; // 1: msg id
+    m->u32HdrSize = 2;
     m->u32PayloadSize = 0;
-    m->result = 0;
+    m->pu8Payload = NULL;
 }
 
-static void cht_ipc_hdr_free(stChtIpcHdr *m)
+static void cht_ipc_msg_free(stChtIpcMsg *m)
 {
     if (!m) return ;
 
-    m->result = 0;
-    m->u16Id = 0xFFFF;
-    m->u16CmdType = 0xFFFF;
+    m->u32FourCC = CHT_IPC_FOURCC;
+    m->u32HdrSize = 0;
     m->u32PayloadSize = 0;
 }
 
-static int cht_ipc_hdr_checkFourCC(uint32_t u32FourCC)
+static int cht_ipc_msg_checkFourCC(uint32_t u32FourCC)
 {
-    printf("%x %x\n", u32FourCC, CHT_IPC_FOURCC);
     return ((u32FourCC == CHT_IPC_FOURCC)?1:0);
 }
 
@@ -61,12 +64,12 @@ static int cht_ipc_hdr_checkFourCC(uint32_t u32FourCC)
 #ifdef __cplusplus
 namespace llt {
 
-struct ChtIpcHdr {
-    stChtIpcHdr m{};
-    ChtIpcHdr(uint16_t u16Id, uint16_t u16CmdType) { 
-        cht_ipc_hdr_init(&m, u16Id, u16CmdType); 
+struct ChtIpcMsg {
+    stChtIpcMsg m{};
+    ChtIpcMsg(uint16_t u16MsgId, uint16_t u16CmdType) { 
+        cht_ipc_msg_init(&m, u16MsgId, u16CmdType); 
     }
-    ~ChtIpcHdr() { cht_ipc_hdr_free(&m); }
+    ~ChtIpcMsg() { cht_ipc_msg_free(&m); }
 };
 
 } // namespace llt
